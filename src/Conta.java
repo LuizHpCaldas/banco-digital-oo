9d5e710bfce32702455617fddc9def31636e1c26
Usuario <luizhpcaldas@gmail.com>
2024-05-22 19:51:33 -0300

    initial commit

---
+++ src/Conta.java
@@ -0,0 +1,36 @@
+public abstract class Conta {
+    private String numero;
+    private double saldo;
+
+    public Conta(String numero) {
+        this.numero = numero;
+        this.saldo = 0.0;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0 && saldo >= valor) {
+            saldo -= valor;
+        }
+    }
+
+    public void transferir(Conta contaDestino, double valor) {
+        if (valor > 0 && saldo >= valor) {
+            this.sacar(valor);
+            contaDestino.depositar(valor);
+        }
+    }
+}
